0ab837c4e5ee53b305504c36add268ca5c07bbae
Jon <jon@apportunities.ca>
2026-04-28 21:14:54 -0600

    Add story for testing 1099NEC

--- src/stories/Default.stories.tsx
+++ src/stories/Default.stories.tsx
@@ -1,6 +1,6 @@
 import { ReactSpreadsheetImport } from "../ReactSpreadsheetImport"
 import { Box, Link, Code, Button, useDisclosure } from "@chakra-ui/react"
-import { mockRsiValues } from "./mockRsiValues"
+import { mockRsiValues, mock1099NECValues } from "./mockRsiValues"
 import { useState } from "react"
 import type { Result } from "src/types"
 import { RsiProps } from "src/types"
@@ -35,6 +35,54 @@ export const Basic = (args: RsiProps<string>) => {
       </Link>
       <ReactSpreadsheetImport
         {...mockRsiValues}
+        {...args}
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={setData}
+      />
+      {!!data && (
+        <Box pt={64} display="flex" gap="8px" flexDirection="column">
+          <b>Returned data (showing first 100 rows):</b>
+          <Code
+            display="flex"
+            alignItems="center"
+            borderRadius="16px"
+            fontSize="12px"
+            background="#4A5568"
+            color="white"
+            p={32}
+          >
+            <pre>
+              {JSON.stringify(
+                {
+                  validData: data.validData.slice(0, 100),
+                  invalidData: data.invalidData.slice(0, 100),
+                  all: data.all.slice(0, 100),
+                },
+                undefined,
+                4,
+              )}
+            </pre>
+          </Code>
+        </Box>
+      )}
+    </>
+  )
+}
+
+export const F1099NEC = (args: RsiProps<string>) => {
+  const [data, setData] = useState<Result<any> | null>(null)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box py={20} display="flex" gap="8px" alignItems="center">
+        <Button onClick={onOpen} border="2px solid #7069FA" p="8px" borderRadius="8px">
+          Open Flow
+        </Button>
+        (make sure you have a file to upload)
+      </Box>
+      <ReactSpreadsheetImport
+        {...mock1099NECValues}
         {...args}
         isOpen={isOpen}
         onClose={onClose}
